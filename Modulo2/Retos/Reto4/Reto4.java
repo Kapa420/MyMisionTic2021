5c18ce102dfd202aa4bdf3f2877d4ae604e77b82
Carlos <arevaloparracarlos@gmail.com>
2021-08-03 22:09:40 +0300

    Codigo Reto 4: Colecciones. Main Class

---
+++ Modulo2/Retos/Reto4/Reto4.java
@@ -0,0 +1,14 @@
+import java.util.TreeSet;
+import java.util.SortedSet;
+
+public class Reto4{
+
+  public static void main(String[] args) {
+    Usuario u = new Usuario();
+    u.addTelefono("333333");
+    u.addTelefono("111111");
+    u.addTelefono("222222");
+    u.addTelefono("222222");
+    u.mostrarTelefonos();
+  }
+}
