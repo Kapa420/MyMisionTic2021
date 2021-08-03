8798cd916cce7120f86ccd901627679e420bbedb
Carlos <arevaloparracarlos@gmail.com>
2021-08-03 20:56:10 +0300

    Codigo clase 20/07 main class

---
+++ Modulo2/Clases/clase2007/ejercicio2/ejercicio2.java
@@ -0,0 +1,12 @@
+
+public class ejercicio2{
+  public static void main(String[] args) {
+    paciente paciente1 = new paciente();
+    paciente1.setedad(215);
+    paciente1.setnombre("Horacio");
+    paciente1.setRH('3');
+    paciente1.settipoSangre("H");
+    paciente1.setgenero("Masculina");
+
+  }
+}
