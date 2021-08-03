cc9e708632ea3cfce8d2172777ba010d0eb46071
Carlos <arevaloparracarlos@gmail.com>
2021-08-03 21:26:27 +0300

    Codigo clase28/07 persona class

---
+++ Modulo2/Clases/clase2807/persona.java
@@ -0,0 +1,14 @@
+public class persona{
+  String nombre;
+  int edad;
+  //Sexo:
+  //Hombre -> 1
+  //Mujer -> 0
+  boolean sexo;
+  persona(String nombre, int edad, boolean sexo)
+  {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.sexo = sexo;
+  }
+}
