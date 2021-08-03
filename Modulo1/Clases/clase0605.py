6c96130a76e5f28a0f89aa0e44af4c45af25ab7c
Carlos <arevaloparracarlos@gmail.com>
2021-08-03 20:47:53 +0300

    Codigo clase 06/05

---
+++ Modulo1/Clases/clase0605.py
@@ -0,0 +1,8 @@
+"""Clase 06/05/2021."""
+# El ejercicio es multiplicar un numero por 2
+num = int(input('Ingrese un numero: '))
+num = 2 * num
+print('Su numero multiplicado es: ', num)
+name = input('Ingrese su nombre: ')
+lastname = input('Ingrese su apellido: ')
+print("Su nombre completo es:", name, lastname)
