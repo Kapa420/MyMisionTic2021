42941540de12e1ab6919fc6a535b3c516bbceff8
Carlos <arevaloparracarlos@gmail.com>
2021-08-03 22:06:08 +0300

    Codigo Reto 3: Relaciones entre clases. Clase alimento

---
+++ Modulo2/Retos/Reto3/Alimento.java
@@ -0,0 +1,12 @@
+public class Alimento{
+  private double gramos; //peso esta en GRAMOS
+  public void setGramos(double gramos){
+    this.gramos = gramos;
+  }
+  public double getGramos(){
+    return this.gramos;
+  }
+  Alimento(double gramos){
+    this.gramos = gramos;
+  }
+}
